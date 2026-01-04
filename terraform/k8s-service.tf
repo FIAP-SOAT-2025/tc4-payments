36d73fa7c31b707271f2b1bfd0a6b58daf2ed475
DianaBianca <dianabiancasr@gmail.com>
2026-01-04 12:55:36 -0300

    ajustes

--- terraform/k8s-service.tf
+++ terraform/k8s-service.tf
@@ -1,3 +1,25 @@
+resource "kubernetes_service" "api_service" {
+  metadata {
+    name      = "api-service"
+    namespace = "tc4-payments"
+    annotations = {
+      "service.beta.kubernetes.io/aws-load-balancer-type" = "nlb"
+    }
+  }
+  spec {
+    selector = {
+      app = "tc4-payments-api"
+    }
+    port {
+      protocol    = "TCP"
+      port        = 80
+      target_port = 3000
+    }
+    type = "LoadBalancer"
+  }
+  depends_on = [kubernetes_namespace.lanchonete_ns]
+}
+
 resource "kubernetes_service" "api_service_internal" {
   metadata {
     name      = "api-service-internal"
